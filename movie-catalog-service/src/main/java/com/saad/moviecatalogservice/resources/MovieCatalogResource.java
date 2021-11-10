03c2212ed60f8ee994e309443b3464614f531040
Saad Shaikh <saadshk111@gmail.com>
2021-11-10 23:26:36 +0530

    Hard-code the ratings in Movie Catalog

--- movie-catalog-service/src/main/java/com/saad/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/saad/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,16 @@
 package com.saad.moviecatalogservice.resources;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saad.moviecatalogservice.models.CatalogItem;
+import com.saad.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +18,14 @@ public class MovieCatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(
-				new CatalogItem("John Wick Chapter 1", "Test Desc", 5)
+		List<Rating> ratings = Arrays.asList(
+					new Rating("123", 5), 
+					new Rating("456", 3)
 				);
+		
+		return ratings
+				.stream()
+				.map(rating -> new CatalogItem("John Wick Chapter 1", "Test Desc", 5))
+				.collect(Collectors.toList());
 	}
 }
