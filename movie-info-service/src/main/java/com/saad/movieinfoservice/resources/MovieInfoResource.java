466246d310fc27c13bf191bdd3e641962315c3b1
Saad Shaikh <saadshk111@gmail.com>
2021-11-12 23:09:43 +0530

    Make external API call for movie info

--- movie-info-service/src/main/java/com/saad/movieinfoservice/resources/MovieInfoResource.java
+++ movie-info-service/src/main/java/com/saad/movieinfoservice/resources/MovieInfoResource.java
@@ -1,17 +1,28 @@
 package com.saad.movieinfoservice.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.saad.movieinfoservice.models.Movie;
+import com.saad.movieinfoservice.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoResource {
+	
+	@Value("${api.key}")
+	private String api_key;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-		return new Movie(movieId, "Test Name for " + movieId);
+		MovieSummary movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + api_key, MovieSummary.class);
+		return new Movie(movieId, movie.getTitle(), movie.getOverview());
 	}
 }
