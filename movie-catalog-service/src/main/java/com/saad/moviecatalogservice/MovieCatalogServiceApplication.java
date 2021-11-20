eef165ebc24a153d5713f990176266782e9a251f
Saad Shaikh <saadshk111@gmail.com>
2021-11-20 19:25:59 +0530

    Add Hystrix for better fault tolerance

--- movie-catalog-service/src/main/java/com/saad/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/saad/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,14 @@ package com.saad.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 	
 	@Bean
--- movie-catalog-service/src/main/java/com/saad/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/saad/moviecatalogservice/resources/MovieCatalogResource.java
@@ -7,35 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.saad.moviecatalogservice.models.CatalogItem;
-import com.saad.moviecatalogservice.models.Movie;
 import com.saad.moviecatalogservice.models.UserRating;
+import com.saad.moviecatalogservice.services.MovieInfo;
+import com.saad.moviecatalogservice.services.UserRatingInfo;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	UserRatingInfo userRatingInfo;
+	
+	@Autowired
+	MovieInfo movieInfo;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
 		// get all movie ratings for given userId
-		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+		UserRating ratings = userRatingInfo.getUserRating(userId);
 		
 		return ratings
 				.getUserRatings()
 				.stream()
-				.map(rating -> {
-					// for each movieId, get movie info
-					Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-					
-					// put it all together
-					return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-				})
+				.map(rating -> movieInfo.getMovieInfo(rating))
 				.collect(Collectors.toList());
 	}
 }
